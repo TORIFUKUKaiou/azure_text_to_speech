bbece4ed3a671c082d762423e740726c4e25e628
Kian-Meng Ang <kianmeng@cpan.org>
2021-03-28 22:36:03 +0800

    Prepare to Hex publishing
    This PR updates the Mix Project and ExDoc config for publishing to
    Hex.pm.

--- mix.exs
+++ mix.exs
@@ -1,17 +1,21 @@
 defmodule AzureTextToSpeech.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/TORIFUKUKaiou/azure_text_to_speech"
+  @version "0.1.0"
+
   def project do
     [
       app: :azure_text_to_speech,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,13 +23,34 @@ defmodule AzureTextToSpeech.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:httpoison, "~> 1.8"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      description: "Elixir wrapper for Azure Text to Speech API.",
+      files: ["lib", "mix.exs", "README.md", "CHANGELOG.md", "LICENSE"],
+      maintainers: ["TORIFUKU Kaiou"],
+      licenses: ["MIT"],
+      links: %{
+        "Changelog" => "https://hexdocs.pm/azure_text_to_speech/changelog.html",
+        "GitHub" => @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["CHANGELOG.md", "README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "#{@version}",
+      formatters: ["html"]
     ]
   end
 end
